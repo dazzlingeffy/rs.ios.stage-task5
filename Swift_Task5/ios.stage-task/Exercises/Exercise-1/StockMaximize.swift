acafd3eadf53a6e915585f45023499f5e43842a5
dazzlingeffy <irusya9@mail.ru>
2021-06-13 20:11:26 +0300

    feat: StockMaximize ✅

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,28 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        if prices.count < 2 {
+            return 0
+        }
+        var pricesCopy = prices
+        var profit = 0
+        var stocks = 0
+        if pricesCopy.firstIndex(of: pricesCopy.max()!) == 0 {
+            pricesCopy.remove(at: 0)
+        }
+        var max = pricesCopy.max()
+        
+        for (i, element) in pricesCopy.enumerated() {
+            if (element != max) {
+                stocks += 1
+                profit -= element
+            } else {
+                profit += stocks * element
+                stocks = 0
+                pricesCopy.remove(at: pricesCopy.firstIndex(of: max!)!)
+                max = pricesCopy.max()
+            }
+        }
+        return profit < 0 ? 0 : profit
     }
 }
