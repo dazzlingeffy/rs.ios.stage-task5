01aa452fd25d2348d6f6e6770f2d3629fe43a89a
dazzlingeffy <irusya9@mail.ru>
2021-06-14 21:14:34 +0300

    feat: MessageDecryptor start

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,25 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var result = ""
+        var number = 0
+        var wasNum = false
+        for char in message {
+            if char.isLetter {
+                result += String(char)
+            } else if char.isNumber {
+                wasNum = true
+                if wasNum {
+                    number *= 10
+                }
+                number += Int(String(char))!
+            } else if char == "[" {
+                wasNum = false
+                while number > 0 {
+                    number -= 1
+                }
+            }
+        }
+        return result
     }
 }
